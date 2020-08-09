74c6e0781183fd5e2ed797ff9229d2c63190c21d
zacht359 <as39dl@yahoo.co.jp>
2020-08-09 13:26:21 +0900

    Add files via upload

---
+++ profiles.js
@@ -0,0 +1,6 @@
+const profiles = [
+    //表向きのプロフィール
+    {name:"withhold private name", age:35, occupation:"TrafficGuide"},
+    //こちらでのプロフィール
+    {name:"zacht", age:35, occupation:"blogger", url:"https://lolipoptest.space"}
+];
