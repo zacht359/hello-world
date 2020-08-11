ecee3481da2962eeea0ad7894142ea7d0900e5bd
zacht359 <as39dl@yahoo.co.jp>
2020-08-11 18:51:02 +0900

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,17 @@
+const add = (num1,num2) => {
+  console.log(num1 + num2);
+};  //足し算の計算式を関数addとして宣言
+const sub = (num1,num2) => {
+  console.log(num1 - num2);
+};  //引き算の計算式を関数subとして宣言
+const mlp = (num1,num2) => {
+  console.log(num1 * num2);
+};  //かけ算の計算式を関数mlpとして宣言
+const div = (num1,num2) => {
+  console.log(num1 / num2);
+};  //割り算の計算式を関数divとして宣言
+//以下、num1に8、num2に2を代入して出力（この値はあとで変更可能）
+add(8,2);
+sub(8,2);
+mlp(8,2);
+div(8,2);
